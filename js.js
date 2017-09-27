b7a620b607325b55728f72195d48cf51485d541a
Zeroble <dlalsdyd0219@gmail.com>
2017-09-28 02:18:48 +0900

    first commit

---
+++ js.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express()
+server = require('http').Server(app);
+var io = require('socket.io').listen(server)
+server.listen(80)
+
+app.get('/',(req,res)=>{
+  console.log("connected");
+  res.sendfile(__dirname+'/html.html')
+});
+
+io.on('connect',(socket)=>{
+  socket.on('room',(data)=>{
+    console.log(data)
+    var arr = Object.keys(io.sockets.connected)
+    socket.broadcast.emit('room',data)
+  })
+})
