e4f17e97d3030a6d3a3f67910210da40ae68ea8a
Zeroble <dlalsdyd0219@gmail.com>
2019-08-09 16:14:33 +0900

    asdf

--- res/skill.js
+++ res/skill.js
@@ -37,8 +37,8 @@ class throwSkill extends skill {
         super(effect,coolTime,skillTypes.THROW)
         this.x = x
         this.y = y-imgWdith//현재 좌표
-        this.tmpX = x
-        this.tmpY = y//임시 좌표
+        this.tmpX = this.x
+        this.tmpY = this.y//임시 좌표
         this.mX = mX
         this.mY = mY//이동할 거리
         this.img = new Image()
@@ -52,16 +52,16 @@ class throwSkill extends skill {
         context.drawImage(this.img, this.x, this.y)
     }
     move(){
-        this.x += (this.mX*this.SPEED);
-        this.y += (this.mY*this.SPEED);
+        this.tmpX += (this.mX*this.SPEED);
+        this.tmpY += (this.mY*this.SPEED);
     }
 }
 
 class fireball extends throwSkill{
     constructor(x,y,mx,my){
         if(mx>=0)
-            super(x,y,mx,my,"res/fireball_right.png",40,15,1000,damage(10));
+            super(x,y,mx,my,"res/fireball_right.png",40,15,1000,damage(1000));
         else
-            super(x,y,mx,my,"res/fireball_left.png",40,15,1000,damage(10));
+            super(x,y,mx,my,"res/fireball_left.png",40,15,1000,damage(1000));
     }
 }
